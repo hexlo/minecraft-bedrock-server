d4d12b3b02bcd740a4124f75d7285801dda1b140
hexlo <hexlo@runx.io>
2024-10-24 07:15:38 -0400

    Update URL to linux version

--- .scripts/download-latest-version.sh
+++ .scripts/download-latest-version.sh
@@ -2,11 +2,11 @@
 # First Argument is the output location. Defaults to current directory
 [[ -z ${1} ]] && dest=. || dest=${1%/}
 
-URL=https://www.minecraft.net/bedrockdedicatedserver/bin-win/bedrock-server-
+URL=https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-
 
 LATEST_VERSION=$(curl -L --silent -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \
     https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 \
-    | grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-win/bedrock-server-[^"]*' \
+    | grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-[^"]*' \
     | sed 's#.*/bedrock-server-##' \
     | sed 's/\.zip//')
 curl -L --silent -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \

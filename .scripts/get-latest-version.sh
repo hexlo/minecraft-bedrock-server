ba9d48393f80427cfbb0e1c11cc1871d6d8d5451
hexlo <hexlo@runx.io>
2024-10-24 07:15:59 -0400

    Update URL to linux version

--- .scripts/get-latest-version.sh
+++ .scripts/get-latest-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl -v -L --silent -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \
 https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 \
-| grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-win/bedrock-server-[^"]*' \
+| grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-[^"]*' \
 | sed 's#.*/bedrock-server-##' | sed 's/.zip//'

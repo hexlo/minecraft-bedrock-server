f758bf2e923b0b0ac12311d745b783ef9209d5ca
hexlo <hexlo@runx.io>
2025-10-31 08:07:47 -0400

    fix: using script to download latest version

--- .scripts/download-latest-version.sh
+++ .scripts/download-latest-version.sh
@@ -8,11 +8,7 @@ SCRAPE_URL=https://minecraft.wiki/w/Bedrock_Dedicated_Server
 # Second Argument is a fixed version to download (optional). If omitted, it'll scrape the webpage to find the latest version.
 if [[ -z ${2} ]]
 then
-    LATEST_VERSION=$(curl -v -L --silent -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \
-    ${SCRAPE_URL} 2>&1 \
-    | grep -o '<li><b><a href="/w/Bedrock_Dedicated_Server_[^"]*' \
-    | tail -1 \
-    | sed 's#.*/Bedrock_Dedicated_Server_##')
+    LATEST_VERSION=$(./.scripts/get-latest-version.sh)
 else
     LATEST_VERSION=${2}
 fi;
@@ -22,4 +18,4 @@ curl -L --silent -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
     && unzip -qq -o ${dest}/bedrock-server.zip -d ${dest} \
     && rm ${dest}/bedrock-server.zip
 
-echo ${LATEST_VERSION}
+echo "Downloaded Minecraft Server version: ${LATEST_VERSION}"

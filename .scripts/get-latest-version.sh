40d17414db38ff6c4c3342c6bdcc59b72ee717ed
hexlo <hexlo@runx.io>
2025-10-31 05:07:49 -0400

    Fixed grep command to get release version

--- .scripts/get-latest-version.sh
+++ .scripts/get-latest-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -v -L --silent -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \
 https://minecraft.wiki/w/Bedrock_Dedicated_Server 2>&1 \
-| grep -o '<li><b><a href="/w/Bedrock_Dedicated_Server_[^"]*' \
+| grep -o '^.*Release:.*Bedrock_Dedicated_Server_[^"]*' \
 | tail -1 \
 | sed 's#.*/Bedrock_Dedicated_Server_##'
